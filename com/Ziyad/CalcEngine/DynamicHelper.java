c10784a595d10b4d25cca2e8a343a711e005a861
Ziyad Tarek <ziyad.tarek.ms@alexu.edu.eg>
2020-04-21 12:57:47 +0200

    Now user can input the calculations

--- com/Ziyad/CalcEngine/DynamicHelper.java
+++ com/Ziyad/CalcEngine/DynamicHelper.java
@@ -2,32 +2,35 @@ package com.Ziyad.CalcEngine;
 
 public class DynamicHelper {
     private MathProcessing[] handlers;
-    private double leftVal, rightVal;
 
     public DynamicHelper(MathProcessing[] handlers){
         this.handlers = handlers;
     }
 
-    public String process(String statement) throws InvalidStatementException{
+    public String process(String statement) throws InvalidStatementException {
         String[] parts = statement.split(MathProcessing.SEPARATOR);
-        if(parts.length != 3)
-            throw new InvalidStatementException("Incorrect number of fields", statement);
+
+        if (parts.length != 3)
+                throw new InvalidStatementException("Incorrect number of fields", statement);
         String keyword = parts[0];
 
         MathProcessing theHandler = null;
-        for(MathProcessing handler: handlers){
-            if (keyword.equalsIgnoreCase(handler.getKeyword())){
+        for (MathProcessing handler : handlers) {
+            if (keyword.equalsIgnoreCase(handler.getKeyword())) {
                 theHandler = handler;
                 break;
             }
         }
         if (theHandler == null)
             throw new InvalidStatementException("Invalid command", statement);
+
+        double rightVal;
+        double leftVal;
         try {
             leftVal = Double.parseDouble(parts[1]);
             rightVal = Double.parseDouble(parts[2]);
         }
-        catch (NumberFormatException e){
+        catch (NumberFormatException e) {
             throw new InvalidStatementException("Non-numeric data", statement, e);
         }
         double result = theHandler.doCalculation(leftVal, rightVal);
--- com/Ziyad/MyApp/Main.java
+++ com/Ziyad/MyApp/Main.java
@@ -1,16 +1,15 @@
 package com.Ziyad.MyApp;
 
 import com.Ziyad.CalcEngine.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String[] statements = {
-                "add 25.0 92.0",
-                "power 0.0 0.0",
-                "divide 100.0 0.0",
-                "root 2.0 25.0"
-        };
+        Scanner input = new Scanner(System.in);
+        System.out.println("Usage:");
+        System.out.println("available functions: add - subtract - multiply - divide - power - root");
+        System.out.println("input example: multiply 63.0 85.0");
         DynamicHelper helper = new DynamicHelper(new MathProcessing[] {
                 new Adder(),
                 new Subtracter(),
@@ -19,10 +18,13 @@ public class Main {
                 new PowerOf(),
                 new Root(),
         });
-        for (String statement: statements) {
+        String statement = input.nextLine();
+        while(!statement.equalsIgnoreCase("q")){
             try {
                 String output = helper.process(statement);
                 System.out.println(output);
+                System.out.println("type q to exit or enter another statement:");
+                statement = input.nextLine();
             }
             catch (InvalidStatementException e){
                 System.out.println(e.getMessage());
