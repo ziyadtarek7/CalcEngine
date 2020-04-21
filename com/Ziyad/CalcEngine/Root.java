bcce67a2a423c25bd8b9303e377a76d2c1059daa
Ziyad Tarek <ziyad.tarek.ms@alexu.edu.eg>
2020-04-21 13:03:58 +0200

    fixed variable name

--- com/Ziyad/CalcEngine/Root.java
+++ com/Ziyad/CalcEngine/Root.java
@@ -13,7 +13,7 @@ public class Root implements MathProcessing {
 
     @Override
     public double doCalculation(double leftVal, double rightVal) throws InvalidStatementException {
-        if (leftVal < 0)
+        if (rightVal < 0)
             throw new InvalidStatementException("Root of negative value is not real");
         return Math.pow(rightVal, 1/leftVal);
     }
